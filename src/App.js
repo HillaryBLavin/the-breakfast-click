70a07741c199b80306e4d1238ba11e553915ca76
Hillary Lavin <hilavin@gmail.com>
2018-06-28 13:26:28 -0400

    Pseudocode (to be distributed later

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Score from "./components/Score";
+import Jumbotron from "./components/Jumbotron";
+import Container from "./components/Wrapper";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Header>
+        {/* <Header>
           <Score></Score>
         </Header>
         <Jumbotron></Jumbotron>
         <Container>
           <Card>
           </Card>
-        </Container>
+        </Container> */}
       </Wrapper>
     );
   }

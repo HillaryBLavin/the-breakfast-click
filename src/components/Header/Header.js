f01120ce85dcb8197d66e7bce4688115c6a6e7ac
Hillary Lavin <hilavin@gmail.com>
2018-06-28 19:13:52 -0400

    Pass props to Header to display message and scores

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,115 +1,26 @@
 import React from 'react';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem
  } from 'reactstrap';
 // import Score from "./components/Score";
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-        <Navbar color="light" light expand="md" className="sticky-top">
-          <NavbarBrand href="/">The Breakfast Click</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="mx-auto" navbar>
-              <NavItem className="text-nowrap flex-row mx-md-auto order-1 order-md-2">
-                Click on a character to begin!
-              </NavItem>
-            </Nav>
-            <Nav>
-              <NavItem>
-                {/* <Score /> */}
-                This is your score!
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-
-    );
-  }
-}
-
-// import React from 'react';
-// import {
-//   Collapse,
-//   Navbar,
-//   NavbarToggler,
-//   NavbarBrand,
-//   Nav,
-//   NavItem,
-//   NavLink,
-//   UncontrolledDropdown,
-//   DropdownToggle,
-//   DropdownMenu,
-//   DropdownItem } from 'reactstrap';
-
-// export default class Example extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.toggle = this.toggle.bind(this);
-//     this.state = {
-//       isOpen: false
-//     };
-//   }
-//   toggle() {
-//     this.setState({
-//       isOpen: !this.state.isOpen
-//     });
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <Navbar color="light" light expand="md">
-//           <NavbarBrand href="/">reactstrap</NavbarBrand>
-//           <NavbarToggler onClick={this.toggle} />
-//           <Collapse isOpen={this.state.isOpen} navbar>
-//             <Nav className="ml-auto" navbar>
-//               <NavItem>
-//                 <NavLink href="/components/">Components</NavLink>
-//               </NavItem>
-//               <NavItem>
-//                 <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-//               </NavItem>
-//               <UncontrolledDropdown nav inNavbar>
-//                 <DropdownToggle nav caret>
-//                   Options
-//                 </DropdownToggle>
-//                 <DropdownMenu right>
-//                   <DropdownItem>
-//                     Option 1
-//                   </DropdownItem>
-//                   <DropdownItem>
-//                     Option 2
-//                   </DropdownItem>
-//                   <DropdownItem divider />
-//                   <DropdownItem>
-//                     Reset
-//                   </DropdownItem>
-//                 </DropdownMenu>
-//               </UncontrolledDropdown>
-//             </Nav>
-//           </Collapse>
-//         </Navbar>
-//       </div>
-//     );
-//   }
-// }
+const Header = props => (
+  <Navbar color="light" light expand="md" className="sticky-top">
+  <NavbarBrand href="/">The Breakfast Click</NavbarBrand>
+    <Nav className="mx-auto" navbar>
+      <NavItem className="text-nowrap flex-row mx-md-auto order-1 order-md-2">
+        {props.message}
+      </NavItem>
+    </Nav>
+    <Nav>
+      <NavItem>
+        Your Score: {props.score} Top Score: {props.topScore}
+      </NavItem>
+    </Nav>
+</Navbar>
+);
+  
+export default Header;

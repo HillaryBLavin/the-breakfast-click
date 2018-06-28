332bb23d9ed7b0a176db75c56d12eb343c055a0d
Hillary Lavin <hilavin@gmail.com>
2018-06-28 13:57:44 -0400

    Simplify component tree, clean up imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import Score from "./components/Score";
 import Jumbotron from "./components/Jumbotron";
-import Container from "./components/Wrapper";
+import Container from "./components/Container";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Wrapper>
-        {/* <Header>
-          <Score></Score>
-        </Header>
-        <Jumbotron></Jumbotron>
-        <Container>
-          <Card>
-          </Card>
-        </Container> */}
+        <Header />
+        <Jumbotron />
+        <Container />
       </Wrapper>
     );
   }

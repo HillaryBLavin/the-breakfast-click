2c5f465bfe142d4a214166ffd2a85ab5db965688
Hillary Lavin <hilavin@gmail.com>
2018-06-28 20:10:58 -0400

    Add Header props, handleClick(), and state key/values

--- src/App.js
+++ src/App.js
@@ -6,17 +6,45 @@ import characters from "./characters.json"
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
+      guessArray: [],
+      message: "Click a character to begin!",
       score: 0,
-      characters: characters
+      topScore: 0
     };
-  }
+
+    handleClick = card => {
+      let guessArray = this.state.guessArray;
+      let score = this.state.score;
+
+      // If they click the same card more than once...
+      if(guessArray[card.id]) { 
+        // Set State - restart game
+        this.setState({
+          message: "You already chose them! Back to Start!",
+          topScore: Math.max(this.state.score, this.state.topScore),
+          guessArray: [],
+          score: 0
+        })
+        // Otherwise...
+      } else {
+        guessArray[card.id] = true;
+        this.setState({
+          message: "Nice! Keep Clicking...",
+          guessArray: guessArray,
+          score: ++score
+        })
+      }
+    };
+  
   render() {
     return (
       <div>
-        <Header />
+        <Header 
+          message={this.state.message}
+          score={this.state.score}
+          topScore={this.state.topScore}
+        />
         <Title />
         <CardWrapper 
           characters={this.state.characters}
